6330470da06c96c6e4be1473dc68fae43fbdc937
yang <2061201881@qq.com>
2019-12-02 15:13:24 +0800

    注释

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+*{
+  padding:0px;
+  margin:0px;
+  box-sizing:border-box;
+}
+html,body,#app{
+  width:100%;
+  height:100%;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="da">
+         <div class="shang">
+             我是Home
+         </div>
+         <div class="zhong">
+             中部
+         </div>
+         <div class="xia">
+             下部
+         </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang='scss' scoped>
+.da{
+    width:100%;
+    height:100%;
+    display:flex;
+    flex-direction:column;
+}
+.shang{
+    width:100%;
+    height:50px;
+    background:blue;
+}
+.zhong{
+    flex:1;
+}
+.xia{
+    width:100%;
+    height:50px;
+    background:blue;
+}
+</style>
